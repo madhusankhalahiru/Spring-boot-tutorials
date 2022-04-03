4970c747af619e7893b3255c9f29629ae4acb74d
madhusankhalahiru <madhusankhalahiru@gmail.com>
2022-04-03 13:00:30 +0530

    Initial commit

---
+++ src/test/java/com/tutorials/rest/RestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorials.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
